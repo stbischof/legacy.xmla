17bc9123570de94d04e370dba81f26114989d6c9
Stefan Bischof <stbischof@bipolis.org>
2024-10-15 17:59:13 +0200

    move mdx to eclipse repo
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

--- mdx/model.record/src/main/java/org/eclipse/daanse/mdx/model/record/select/CreateCellCalculationBodyClauseR.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.daanse.mdx.model.record.select;
-
-import org.eclipse.daanse.mdx.model.api.select.CreateCellCalculationBodyClause;
-
-public record CreateCellCalculationBodyClauseR() implements CreateCellCalculationBodyClause {
-
-}
